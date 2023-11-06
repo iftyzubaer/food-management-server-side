7c834148c125fe13edc8614a6b6db5589710c543
iftyzubaer <iftyzubaer1304.com>
2023-11-06 19:49:34 +0600

    inserting data in database

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// const uri = "mongodb+srv://<username>:<password>@cluster0.gsq0j5x.mongodb.net/?retryWrites=true&w=majority";
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gsq0j5x.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,6 +23,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const foodCollection = client.db('foodDB').collection('food')
+
+    app.post('/food', async (req, res) => {
+      const newFood = req.body
+      const result = await foodCollection.insertOne(newFood)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -35,9 +43,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Server is running')
+  res.send('Server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on: ${port}`);
+  console.log(`Server is running on: ${port}`);
 })
